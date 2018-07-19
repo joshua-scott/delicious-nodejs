5bdf9aaabda9ab596951a35651b27ddcfce24fd2
Joshua Scott <jscott313@gmail.com>
2018-07-19 17:49:06 +0300

    Implement extremely basic MVC pattern to render homepage

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const storeController = require('../controllers/storeController')
 
-router.get('/', (req, res) => {
-  res.send('Hey! It works!')
-})
+router.get('/', storeController.homePage)
 
 module.exports = router
