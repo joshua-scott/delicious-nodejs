c4171c51d1668290ac47e94a130eb10c6a1eff58
Joshua Scott <jscott313@gmail.com>
2018-07-22 09:31:45 +0300

    Display all stores from the database

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose')
 const Store = mongoose.model('Store')
 
-exports.homePage = (req, res) => {
-  res.render('index')
-}
-
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' })
 }
@@ -16,4 +12,9 @@ exports.createStore = async (req, res) => {
     `Successfully created ${store.name}. Care to leave a review?`
   )
   res.redirect(`store/${store.slug}`)
+}
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find()
+  res.render('stores', { title: 'Stores', stores })
 }
