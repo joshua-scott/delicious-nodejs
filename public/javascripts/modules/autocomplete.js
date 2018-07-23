2cfa135ebb6fdfa7101de0b2564a3313a82ede42
Joshua Scott <jscott313@gmail.com>
2018-07-23 23:06:54 +0300

    Autocomplete lat/lng using Google Maps

---
+++ public/javascripts/modules/autocomplete.js
@@ -0,0 +1,18 @@
+function autocomplete(input, latInput, lngInput) {
+  if (!input) return
+
+  const dropdown = new google.maps.places.Autocomplete(input)
+
+  dropdown.addListener('place_changed', e => {
+    const place = dropdown.getPlace()
+    latInput.value = place.geometry.location.lat()
+    lngInput.value = place.geometry.location.lng()
+  })
+
+  // If someone hits enter on the address field, don't submit the form
+  input.addEventListener('keydown', e => {
+    if (e.keyCode === 13) e.preventDefault()
+  })
+}
+
+export default autocomplete
